d558185c66edbc3feeac102d8824dafe263ad815
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2025-04-04 18:53:32 +0300

    feat: function created to attach session to log-in user

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -1,3 +1,28 @@
+import passport from "../authentication/passportConfig.js";
+
+function authenticateUser(req, res, next) {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.error("Error during authentication:", err);
+      return next(err);
+    }
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: info?.message || "Unauthorized user" });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        console.error("Error during login:", err);
+        return res.status(401).json({ message: "User not authenticated" });
+      }
+      //set session to the current user
+      req.session.user = user;
+      next();
+    });
+  })(req, res, next);
+}
+
 function ensureUserAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -5,4 +30,4 @@ function ensureUserAuthenticated(req, res, next) {
   return res.status(401).json({ message: "unauthorized user" });
 }
 
-export { ensureUserAuthenticated };
+export { ensureUserAuthenticated, authenticateUser };
